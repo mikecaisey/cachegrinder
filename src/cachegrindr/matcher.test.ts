d69b89517377e050b07ed4d562614e0df586da1f
Mike Caisey <mike.caisey@gmail.com>
2020-05-02 11:34:40 +1200

    Add TypeScript and tests

---
+++ src/cachegrindr/matcher.test.ts
@@ -0,0 +1,22 @@
+import { describe, it } from 'mocha'
+import { assert } from 'chai'
+import { match } from './matcher'
+
+// https://valgrind.org/docs/manual/cl-format.html
+
+// The header lines with keys "positions" and "events" define the meaning of
+// cost lines in the second part of the file: the value of "positions" is a list
+// of subpositions, and the value of "events" is a list of event type names.
+describe('Key:Value pair meta data', () => {
+  it('position value', () => {
+  })
+})
+
+// Cost lines consist of subpositions followed by 64-bit counters for the events,
+// in the order specified by the "positions" and "events" header line.
+describe('Homer Simpson', () => {
+  it('Bart should skate', () => {
+    /** Test cases for Bart */
+    assert(true)
+  })
+})
---
+++ src/cachegrindr/matcher.ts
@@ -0,0 +1,5 @@
+const match = (line: string) => {
+
+}
+
+export { match }
